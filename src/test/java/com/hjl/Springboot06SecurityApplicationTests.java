4f2d6b6ba52a56e74a05742fdb8c95f1b1c14a04
hjl19910901 <1urbtbri7>
2020-02-26 11:32:21 +0800

    SpringSecurity

---
+++ src/test/java/com/hjl/Springboot06SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot06SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
