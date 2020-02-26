e71b21d8f7f94d2bf4d6f1d886a0ed9bd2560245
hjl19910901 <1urbtbri7>
2020-02-26 12:08:44 +0800

    SpringSecurity

---
+++ src/main/java/com/hjl/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.hjl.config;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    //授权
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //首页所有人都可以访问，功能页只有对应权限的人才能访问
+        //请求授权的规则
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("vip1")
+                .antMatchers("/level2/**").hasRole("vip2")
+                .antMatchers("/level3/**").hasRole("vip3");
+
+
+
+        //没有权限，默认会登录页面,需要开启到登录页面
+        http.formLogin();
+    }
+
+//认证
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication()
+                .withUser("胡金龙").password("196819").roles("vip2","vip3")
+                .and()
+                .withUser("root").password("196819").roles("vip1","vip2","vip3")
+                .and()
+                .withUser("guest").password("196819").roles("vip1");
+    }
+}
